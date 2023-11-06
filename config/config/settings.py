4818a6c844be4a193ddebc4771fb68af4ce3f582
Min Gyung Park <kateking001130@ewhain.net>
2023-11-06 12:15:27 +0900

    template 경로 설정

--- config/config/settings.py
+++ config/config/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#템플릿 디렉토리 경로 설정
+TEMPLATES_DIR = BASE_DIR/"templates"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
